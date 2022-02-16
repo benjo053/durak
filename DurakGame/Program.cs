71ea154e4c64e24441068f35fa24e1c2aaa47b37
Andrii Mikhno <andrii.mikhno@alumni.fh-aachen.de>
2022-02-16 12:32:40 +0100

    Entwurf

--- DurakGame/Program.cs
+++ DurakGame/Program.cs
@@ -14,4 +14,43 @@ namespace DurakGame
             Application.Run(new Form1());
         }
     }
+
+
+    public class Game
+    {
+        private List<Player> players;
+    }
+
+    public class Round
+    {
+        private Player offender;
+        private Player defender;
+    }
+
+    public class Player
+    {
+        private int id;
+        private string name;
+        private List<Card> hand;
+    }
+
+    public class Pack
+    {
+        private List<Card> cards;
+        private Suit trump_suit;
+    }
+
+    public class Card
+    {
+        private Suit suit;
+    }
+
+    public enum Suit
+    {
+        HEART = 0,
+        DIAMOND = 1,
+        CLUB = 2,
+        SPADE
+
+    }
 }
